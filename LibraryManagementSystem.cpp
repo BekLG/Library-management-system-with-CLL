e39c4460d12a5618a62e97be4458fbb01f370e77
BekLg <bereketlegesse22@gmail.com>
2022-12-30 06:55:40 +0300

    display all and register functions worked

--- LibraryManagementSystem.cpp
+++ LibraryManagementSystem.cpp
@@ -3,37 +3,137 @@ using namespace std;
 
 struct book
 {
-    char title[100];
-   /* char author[100]
-    int publicationYear;
-    char genre[20];
+    string title;
+    string author;
     int numberOfPages;
-    float price;
+  //  string id;
+    int publicationYear;
+    string genre;
+  //  float price;
     int shelfNumber;
-    int availableCopies; */
+    int availableCopies;
 
     book * next;
 };
 
-book *head,*tail,*temp,*current,*q;
+book *head,*tail,*temp,*current,*q,*p;  //declaration of necessary pointers
+string titleGV,authorGV,genreGV;        //declaration of global variables
+int numberOfPagesGV,publicationYearGV,shelfNumberGV,availableCopiesGV;  // GV means Global Variable
+
+
+void displayMenu();   //declaration of functions
+void registerBook();
+void displayAllBooks();
+void displayByGenre();
+void searchByTitle();
+void searchByAuthor();
+void lendBook();
+void returnBook();
+
+
+int main()
+{
+    displayMenu();
+}
 
 void registerBook()
 {
-     cout<<"register book page"<<endl;
+     temp= new book;
+
+    cin.ignore();
+        cout<<"Enter Title Of The Book: ";
+	    getline(cin,titleGV);
+	    cout<<"Enter author Of The Book: ";
+	    getline(cin,authorGV);
+	    cout<<"Enter genre Of The Book: ";
+	    getline(cin,genreGV);
+
+	    cout<<"Enter page Of The Book: ";
+	    cin>>numberOfPagesGV;
+	    cout<<"Enter publication year Of The Book: ";
+	    cin>>publicationYearGV;
+	    cout<<"Enter available copies Of The Book: ";
+	    cin>>availableCopiesGV;
+	    cout<<"Enter location/ shelf number Of The Book: ";
+	    cin>>shelfNumberGV;
+
+
+	     cin.ignore();
+
+
+	    temp->title=titleGV;
+	    temp->author=authorGV;
+	    temp->genre=genreGV;
+	    temp->numberOfPages=numberOfPagesGV;
+	    temp->publicationYear=publicationYearGV;
+	    temp->availableCopies=availableCopiesGV;
+	    temp->shelfNumber=shelfNumberGV;
+
+	  // cin>>book_name;
+
+    if(tail==NULL){//if the list is empty linked list will instantiated.
+               tail=temp;
+               head=temp;
+               tail->next=head;
+            }
+            else{//if the node is already instantiated, the new node will be inserted at a given position.
+                    current=tail;
+                 do{
+                        int res=titleGV.compare(current->title);//compare book name string in the new node with the list to get the position, to make the list is sorted by book name.
+                        q=current->next;
+                    if(res < 0 && current == head){//at the begining.
+                          p=head;
+                          head=temp;
+                          temp->next=p;
+                          p=temp;
+                          tail->next=temp;
+
+                        }
+
+                       else if(res >0){
+
+                               if(current==tail){//At the end.
+                                    temp->next=head;
+                                    tail->next=temp;
+                                    tail=temp;
+
+                               }
+                               if(q->title >= titleGV && res >0){//N_th position
+                                    temp->next=q;
+                                    current->next=temp;
+
+                               }
+                       }
+                       current=current->next;//traverse to all nodes to compare the string.
+
+		           }while(current!=tail);//till the end of the linked list.
+}
+            displayMenu();
+
 }
 
 void displayAllBooks()
 {
     cout<<"display book page"<<endl;
+
+      current=tail->next;
+    do
+    {
+        cout<<current->title<<" "<<current->author<<" "<<current->genre<<" "<<current->numberOfPages<<" "<<current->publicationYear<<" "<<current->availableCopies<<" "<<current->shelfNumber<<endl;
+        current=current->next;
+    }
+    while(current!=tail->next);
+
+    displayMenu();
 }
 
 
 void displayByGenre()
 {
-    string genre;
+
     cout<<"enter the genre you want to display"<<endl;
-    cin>>genre;
-    cout<<"display book page  "<<genre;
+    cin>>genreGV;
+    cout<<"display book page  "<<genreGV;
 }
 
 
@@ -67,6 +167,7 @@ void returnBook()
 
 void displayMenu()
 {
+    system("clear");
     int choice;
     cout<<"welcome to library management System"<<endl<<endl;
     cout<<"enter 1 to register new book"<<endl;
@@ -99,6 +200,7 @@ void displayMenu()
         }
         else if(choice==3)
         {
+            system("clear");
             displayMenu();
         }
         else
@@ -123,6 +225,7 @@ void displayMenu()
         }
         else if(choice==3)
         {
+            system("clear");
             displayMenu();
         }
         else
@@ -152,11 +255,4 @@ void displayMenu()
 }
 
 
-int main()
-{
 
-
-    displayMenu();
-
-
-}
